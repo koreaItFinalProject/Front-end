f4ad35f29a79e78d899ec0c7e0a63982fba4a576
CodeDDAGGI <qudrb1132@naver.com>
2024-10-07 14:11:59 +0900

    Style: "헤더 스타일 뼈대 완성" 헤더 부분 뼈대 완성

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 /** @jsxImportSource @emotion/react */
-import { css as s } from "@emotion/react";
+import * as s from "./style";
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
@@ -20,11 +20,23 @@ function Header() {
     return (
         <div css={s.layout}>
             <div css={s.highBar}>
-                <button onClick={signin}>로그인</button>
-                <button onClick={signup}>회원가입</button>
+                <div>
+                    <button onClick={signin}>로그인</button>
+                    <button onClick={signup}>회원가입</button>
+                </div>
             </div>
             <div css={s.logo}>
-                {/* <button onClick={navigate("/")}>로고</button> */}
+                <div>
+                    <button onClick={()=> navigate("/")}>
+                        <img src="https://img.craiyon.com/2024-10-07/S-BEupxFSh2QESqNqrpyoA.webp" alt='로고'></img>
+                    </button>
+                </div>
+                <div css={s.manubar}>
+                    <button>게시판</button>
+                    <button>리스트</button>
+                    <button>지도</button>
+                    <button>이벤트</button>
+                </div>
             </div>
         </div>
     );
---
+++ src/page/UserSigninpage/style.js
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const selectMember = css`
+    
+`;
