2e73cb04e333c3996295d2a0cf413021e87bc473
hyeon0907 <kimjh597@naver.com>
2024-10-08 10:40:15 +0900

    10-08 디자인 지도 기초디자인 완성

--- src/page/MapPage/MapPage.jsx
+++ src/page/MapPage/MapPage.jsx
@@ -2,7 +2,7 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import { useState } from "react";
-import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, MapTypeControl, ZoomControl, useKakaoLoader } from "react-kakao-maps-sdk";
 
 function MapPage(props) {
     const [loading, error] = useKakaoLoader({
@@ -16,43 +16,127 @@ function MapPage(props) {
     return (
         <div css={s.layout}>
             <div css={s.box}>
-                <div css={s.searchbox}>
-                    <div css={s.header}>
-                        <div css={s.logobox}>
-                            <button>이벤트 이동</button>
-                            LOGO
-                        </div>
-                        <div css={s.inputbox}>
-                            <input type="text" />
-                            <button>확인</button>
-                            <fieldset css={s.radiobutton}>
-                                <label>
-                                    <input type="radio" name="select" />
-                                    전체
-                                </label>
-                                <label>
-                                    <input type="radio" name="select" />
-                                    선택1
-                                </label>
-                                <label>
-                                    <input type="radio" name="select" />
-                                    선택2
-                                </label>
-                                <label>
-                                    <input type="radio" name="select" />
-                                    선택3
-                                </label>
-                                <label>
-                                    <input type="radio" name="select" />
-                                    선택4
-                                </label>
-                            </fieldset>
-                        </div>
+                <div css={s.header}>
+                    <div css={s.logobox}>
+                        <button>이벤트 이동</button>
+                        LOGO
                     </div>
-                    <div css={s.content}>카페리스트</div>
+                    <div css={s.inputbox}>
+                        <input type="text" />
+                        <button>확인</button>
+                        <fieldset css={s.radiobutton}>
+                            <label>
+                                <input type="radio" name="select" />
+                                전체
+                            </label>
+                            <label>
+                                <input type="radio" name="select" />
+                                선택1
+                            </label>
+                            <label>
+                                <input type="radio" name="select" />
+                                선택2
+                            </label>
+                            <label>
+                                <input type="radio" name="select" />
+                                선택3
+                            </label>
+                            <label>
+                                <input type="radio" name="select" />
+                                선택4
+                            </label>
+                        </fieldset>
+                    </div>
+                </div>
+                <div css={s.content}>카페리스트
+                    <div css={s.contentlist}>
+                        <ul>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                            <li>
+                                <p>카페이름</p>
+                                <p>평점</p>
+                                <p>주소</p>
+                                <p>카테고리/ 리뷰수</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <Map css={s.map} center={center}></Map>
+            <Map css={s.map} center={center}>
+                <MapTypeControl position={"TOPRIGHT"} />
+                <ZoomControl position={"RIGHT"} />
+            </Map>
         </div>
     )
 }
