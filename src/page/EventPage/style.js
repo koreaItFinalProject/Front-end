4647e4679e86862d1e525048170c75785966fa9d
burgundy02 <jihyuntina@naver.com>
2024-10-08 09:38:29 +0900

    이벤트 페이지 진행중

--- src/page/EventPage/style.js
+++ src/page/EventPage/style.js
@@ -1,37 +1,37 @@
 import { css } from "@emotion/react";
 
 export const layout = css`
-    width: 100%;
-    height: 100%;
+    /* width: 50%;
+    height: 100%; */
 `;
 
 export const eventImageBox = css`
     box-sizing: border-box;
     border-bottom: 1px solid black;
     width: 100%;
-    height: 45%;
+    height: 400px; 
 `;
 
 export const progressEvent = css`
     box-sizing: border-box;
     width: 100%;
-    height: 15%;
+    height: 15px;
 `;
 
 export const eventPost = css`
-    display: flex;
+    /* display: flex;
     justify-content: center;
     flex-wrap: wrap;
     box-sizing: border-box;
     border-top: 1px solid black;
     width: 100%;
-    height: 40%;
+    height: 40%; */
 
     & > div {
-        box-sizing: border-box;
+        /* box-sizing: border-box;
         border: 1px solid black;
         width: 50%;
-        height: 100px;
+        height: 100px; */
 
     }
 `;
--- src/page/ListPage.jsx
+++ src/page/ListPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ListMain from '../components/ListMain/ListMain';
+import Header from '../components/Header/Header';
 
 function ListPage(props) {
     return (
-       <ListMain></ListMain>
+        <>
+            <Header/>
+            <ListMain/>
+        </>
     );
 }
 
