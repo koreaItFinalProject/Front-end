579dae526fd84226f10279449d8740632bc1d062
burgundy02 <jihyuntina@naver.com>
2024-10-08 16:44:24 +0900

    관리자프로필페이지

---
+++ src/page/Manager/ManagerProfilePage/style.js
@@ -0,0 +1,63 @@
+import { css } from "@emotion/react";
+
+export const mainLayout = css`
+    display: flex;
+    
+`;
+
+export const layout = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    box-sizing: border-box;
+    border: 1px solid #898484;
+    border-top: none;
+    width: 300px;
+    height: 1000px;
+
+    & > span {
+        margin-left: 50px;
+        font-size: 25px;
+    }
+`;
+
+export const listBox = css`
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    
+
+
+`;
+
+export const list = css`
+
+    & > div {
+        display: flex;
+        margin: 20px 0;
+
+        & > p {
+            margin-right: 8px;
+            font-size: 20px;
+        }
+
+        & > input {
+            box-sizing: border-box;
+            height: 40px;
+            border: none;
+            outline: none;
+            color: black;
+            width: 70%; 
+            font-size: 40px;    
+            background-color: white;
+            border-bottom: 1px solid #8f8282;
+        }
+    }
+`;
