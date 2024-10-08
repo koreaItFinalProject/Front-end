68accd91881a20a40884c4c40fab43786fceaf0e
burgundy02 <jihyuntina@naver.com>
2024-10-08 17:48:18 +0900

    관리자페이지대시보드

---
+++ src/page/Manager/ManagerDashBoardPage/style.js
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+
+export const mainLayout = css`
+    display: flex;
+`;
+
+export const layout = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    box-sizing: border-box;
+    border: 1px solid #898484;
+    border-top: none;
+    width: 300px;
+    height: 1000px;
+
+    & > span {
+        margin-left: 50px;
+        font-size: 25px;
+    }
+`;
+
+export const contentBox = css`
+    box-sizing: border-box;
+    border: 1px solid #000000;
+    border-top: none;
+    border-left: none;
+    width: 100%;
+    height: 85px;
+`;
+
+export const box = css`
+    display: flex;
+    flex-wrap: wrap;
+
+    & > div {
+        box-sizing: border-box;
+        border: 1px solid #000000;
+        width: 50%;
+        height: 457.6px;
+    }
+`;
+
+export const mainBox = css`
+    width: 100%;
+    height: 100%;
+`;
