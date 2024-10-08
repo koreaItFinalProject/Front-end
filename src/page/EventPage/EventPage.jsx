2c1d1f9a735b61e69fe613c0f80acaf6bc6f883c
burgundy02 <jihyuntina@naver.com>
2024-10-08 14:40:06 +0900

    관리자 페이지 홈

--- src/page/EventPage/EventPage.jsx
+++ src/page/EventPage/EventPage.jsx
@@ -6,26 +6,27 @@ import Header from '../../components/Header/Header';
 function EventPage(props) {
     return (
         <div>
-            <Header/>
+            <Header />
             <div css={s.layout}>
                 <div css={s.eventImageBox}></div>
 
                 <div css={s.progressEvent}></div>
 
-                <div css={s.eventPost}>
-                    <div css={s.searchBox}>
-                        검색:
-                        <input type="text" />
-                    </div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-             </div>
+                <p css={s.searchBox}>
+                    검색:
+                    <input type="text" />
+                </p>
 
-        </div>
+                <div css={s.eventPost}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+            </div>
 
 
 
