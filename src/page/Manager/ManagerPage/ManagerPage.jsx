e48a7ce68477df463766594e2a4e2f8ebab7d6c6
CodeDDAGGI <qudrb1132@naver.com>
2024-10-12 00:03:17 +0900

    Feat : "디자인 수정"

--- src/page/Manager/ManagerPage/ManagerPage.jsx
+++ src/page/Manager/ManagerPage/ManagerPage.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 /** @jsxImportSource @emotion/react */
 import * as s from './style';
-import Header from '../../../components/Header/Header';
-import { Link } from 'react-router-dom';
+import SideBar from '../../../components/SideBar/SideBar';
 
 function ManagerPage(props) {
     return (
-        <div>
-            <Header />
-            <div css={s.mainLayout}>
-                <div css={s.layout}>
-                    <Link to="/manager/profile">프로필</Link>
-                    <Link to="/manager/dashboard">대시보드</Link>
-                    <Link to="/manager/management">사용자 관리</Link>
-                    <Link to="/manager/storemanagement">점포 관리</Link>
-                    <Link to="/manager/storerequest">점포 등록</Link>
-                    <Link>요청 관리</Link>
-                    <Link>설정</Link>
-                    <Link to="/">로그아웃</Link>
-                </div>
-            </div>
-
+        <div css={s.layout}>
+            <SideBar />
         </div>
     );
 }
