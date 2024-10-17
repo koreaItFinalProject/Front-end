2eac93b7f16dfabfb95dc82d1ee1062a765ed276
ehddls98 <icbku1357@naver.com>
2024-10-17 10:51:18 +0900

    Feat: 무한스크롤 작업중

--- src/page/Board/BoardPage/BoardPage.jsx
+++ src/page/Board/BoardPage/BoardPage.jsx
@@ -1,32 +1,34 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import 'swiper/css'
 import 'swiper/css/pagination';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { IoMdArrowDropleft, IoMdArrowDropright, IoMdHeart } from "react-icons/io";
 import ReactPaginate from 'react-paginate';
 import { instance } from '../../../apis/util/instance';
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import BoardList from "../../../components/BoardList/BoardList";
 
 function BoardPage(props) {
-    const [searchParams] = useSearchParams();
     const [searchValue, setSearchValue] = useState("");
     const [searchFilter, setSearchFilter] = useState("title");
     const [isClick, setIsClick] = useState(false);
     const [selectedButton, setSelectedButton] = useState('공지');
-    const page = searchParams.get("page")
-    const limit = 20;
+    const loadMoreRef = useRef(null);
     const navigate = useNavigate();
+    const limit = 20;
 
-    const boardList = useQuery(
-        ["boardListQuery", page, isClick === true],
-        async () => {
-            const response = await instance.get(`/board/list?page=${page}&limit=${limit}&searchFilter=${searchFilter}&searchValue=${searchValue}`);
-            return response;
-        },
+    const boardList = useInfiniteQuery(
+        ["boardListQuery", isClick === true],
+        async ({ page = 1 }) => await instance.get(`/board/list?page=${page}&limit=${limit}&searchFilter=${searchFilter}&searchValue=${searchValue}`),
         {
+            getNextPageParam: (lastPage, allPage) => {
+                const totalPageCount = lastPage.data.totalCount % limit === 0
+                    ? lastPage.data.totalCount / limit
+                    : Math.floor(lastPage.data.totalCount / limit) + 1
+                return totalPageCount !== allPage.length ? allPage.length + 1 : null;
+            },
             retry: 0,
             refetchOnWindowFocus: false,
             onSuccess: response => {
@@ -34,6 +36,24 @@ function BoardPage(props) {
             }
         }
     );
+
+    console.log(boardList.data);
+
+    useEffect(() => {
+        if (!boardList.hasNextPage || !loadMoreRef.current) {
+            return;
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                boardList.fetchNextPage();
+            }
+        }, { threshold: 1.0 });
+
+        observer.observe(loadMoreRef.current);
+    }, [boardList.hasNextPage]);
+
+
 
     const handleFilterOnChange = (e) => {
         setSearchFilter(e.target.value);
@@ -78,7 +98,7 @@ function BoardPage(props) {
             </div>
             <div css={s.boardListLayout}>
                 <div css={s.boardListHeader}>
-                    <h3>총 {boardList?.data?.data.totalCount}개</h3>
+                    <h3>총 {boardList?.data?.totalCount}개</h3>
                     <select name="searchFilter" onChange={handleFilterOnChange}>
                         <option name="title" value={"title"}>제목</option>
                         <option name="writer" value={"writer"}>작성자</option>
@@ -98,8 +118,9 @@ function BoardPage(props) {
                 </div>
             </div>
             <div css={s.boardList}>
-                <BoardList isClick={isClick} page={page} />
+                <BoardList isClick={isClick} />
             </div>
+            <div ref={loadMoreRef}></div>
         </div>
     );
 }
