4f8979f695ea4ba1e3b5b71f02638a0b77486fa2
CodeDDAGGI <qudrb1132@naver.com>
2024-10-21 23:46:41 +0900

    Feat : "로그인 회원가입 디자인 수정"

--- src/page/SignupPage/UserSignupPage/style.js
+++ src/page/SignupPage/UserSignupPage/style.js
@@ -7,9 +7,11 @@ export const layout = css`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    
+    *{
+        color: #ffffff;
+    }
     & p {
-        margin: 0;
+    margin: 0;
     }
     & > div {
         width: 600px;
@@ -31,33 +33,51 @@ export const Info = css`
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-    & div{
-        position: relative;
-        width: 350px;
-        height: 50px;
-        & input {
-            position: absolute;
-            height: 40px;
-            width: 350px;
-            padding-left: 20px;
-            font-size: 18px;
-        }
-    }
+    flex-direction: column; 
     & p {
         width: 100px;
-        margin-right: 0px;
+        margin: 0px;
         padding: 0px;
-    }    
+    }   
+    & input {
+        padding-left: 10px;
+    }
+
+    & div{
+        padding-bottom: 20px ;
+    }
+`;
+
+export const usernameInput = css`
+    width: 100%;
+    height: 40px;
+    box-sizing: border-box;
+    border-bottom: 1px solid #F2780C;
+    & input {
+        height: 40px;
+        width: 300px;
+        font-size: 18px;
+        border: none;
+    }
+    & button {
+        box-sizing: border-box;
+        height: 30px;
+        width: 80px;
+        border: 1px solid #ffffff;
+        border-radius: 20px;
+    }
 `;
  
 export const emailButton = css`
-    /* display: flex;
+    display: flex;
     justify-content: end;
     align-items: center;
+    width: 380px;
+    box-sizing: border-box;
+    height:60px;
     & button{
         border-radius: 20px;
-        height: 40px;
+        height: 35px;
         width: 100px;
         border: 1px solid black;
     }
@@ -65,40 +85,58 @@ export const emailButton = css`
     & p{
         width: 100px;
         font-size: 14px;
-    } */
-`;
-
-export const addressInfo = css`
-    /* width: 480px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-sizing: border-box;
-    & > div {
-        width: 340px;
     }
-    & p {
-        width: 80px;
-        margin-right: 40px;
-        padding: 10px;
-    } */
 `;
 
-export const addressStyle = css`
-    /* display: flex;
+export const emailcert = css`
+    display: flex;
     flex-direction: column;
-    width: 100px;
-    box-sizing: border-box;
-    & input {    
-    border-radius: 10px;
-    padding-left: 10px;
-    } */
-    
+    width: 100%;
+    height:50px;
+    padding-bottom: 0;
+    margin-bottom: 0;
+    & p {
+        height: 35px;
+        padding-left: 15px;
+    }
+    & div {
+        display: flex;
+        justify-content: end;
+        align-items: end;
+        padding-bottom: 0;
+        margin-bottom: 0;
+        & input{
+            height: 50px;
+        }
+
+        & button {
+            
+        }
+    }
+`;
+
+export const registerButton =css`
+    border-radius: 20px;
+    height: 30px;
+    width: 80px;
+    border: 1px solid black;
 `;
 
 export const signupbutton = css`
-    /* margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    width: 100%;
+    padding: 20px 0px;
     & button {
-        padding: 10px 50px;
-    } */
+        border: 1px solid black;
+        width: 200px;
+        height: 40px;
+        box-sizing: border-box;
+        border-radius: 10px;
+    }
+    button:disabled {
+        cursor: not-allowed;
+    }
 `;
