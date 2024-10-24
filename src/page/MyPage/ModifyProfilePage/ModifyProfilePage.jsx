98fb1c40818d84d0157ed8e0c2e121faaf56a396
CodeDDAGGI <qudrb1132@naver.com>
2024-10-24 17:41:00 +0900

    Feat : "마이페이지 디자인 구성"

--- src/page/MyPage/ModifyProfilePage/ModifyProfilePage.jsx
+++ src/page/MyPage/ModifyProfilePage/ModifyProfilePage.jsx
@@ -9,16 +9,16 @@ import { instance } from '../../../apis/util/instance';
 
 function ModifyProfilePage() {
     const [modifyState, setModifySelect] = useState(true);
-    const [userInfo , setUserInfo] = useState({});
+    const [userInfo, setUserInfo] = useState({});
     const user = useRecoilValue(State);
     const setUser = useSetRecoilState(State);
-    const [modifyUserInfo , setModifyUserInfo] = useState({
-        name : '',
-        username :'',
-        email : '',
-        nickname : '',
-        phoneNumber : '',
-        role:''
+    const [modifyUserInfo, setModifyUserInfo] = useState({
+        name: '',
+        username: '',
+        email: '',
+        nickname: '',
+        phoneNumber: '',
+        role: ''
     })
     const handleChangeOnClick = () => {
         setModifySelect(!modifyState);
@@ -27,7 +27,7 @@ function ModifyProfilePage() {
     const handleOnInput = (e) => {
         setModifyUserInfo({
             ...modifyUserInfo,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
@@ -40,28 +40,28 @@ function ModifyProfilePage() {
             return response.data;
         },
         {
-            enabled: !user.data,  
+            enabled: !user.data,
             onSuccess: (data) => {
-                setUser(data);  
-                setUserInfo(data.user); 
+                setUser(data);
+                setUserInfo(data.user);
             },
         }
     );
 
-    useEffect(()=> {
+    useEffect(() => {
         setModifyUserInfo({
-            name : user.user?.name,
-            username :user.user?.username,
-            email : user.user?.email,
-            nickname : user.user?.nickname,
-            phoneNumber : user.user?.phoneNumber,
-            role:user.user?.role
+            name: user.user?.name,
+            username: user.user?.username,
+            email: user.user?.email,
+            nickname: user.user?.nickname,
+            phoneNumber: user.user?.phoneNumber,
+            role: user.user?.role
         })
         console.log(user);
-    },[user]);
+    }, [user]);
     console.log(user);
 
-    if(!user){
+    if (!user) {
         return <div>Loading...</div>
     }
 
@@ -69,7 +69,7 @@ function ModifyProfilePage() {
         <div css={s.layout}>
             <div css={s.profileBox}>
                 <div>
-                    <button onClick={handleChangeOnClick}>수정하기<FaPencilAlt/></button>
+                    <button onClick={handleChangeOnClick}>수정하기<FaPencilAlt /></button>
                 </div>
                 <div css={s.profileimage}>
                     <img src={userInfo.img} alt="프로필 이미지" />
@@ -79,74 +79,75 @@ function ModifyProfilePage() {
                       이름, 아이디 , 닉네임 , 이메일 수정가능해야함  
                     */}
                     {
-                        modifyState?
-                        <div css={s.profileinfo}>
-                            <div>
-                                <p>이름 :</p>
-                                <p>{userInfo.name}</p>
+                        modifyState ?
+                            <div css={s.profileinfo}>
+                                <div>
+                                    <p>이름 :</p>
+                                    <p>{userInfo.name}</p>
+                                </div>
+                                <div>
+                                    <p>아이디 :</p>
+                                    <p>{userInfo.username}</p>
+                                </div>
+                                <div>
+                                    <p>닉네임 :</p>
+                                    <p>{userInfo.nickname}</p>
+                                </div>
+                                <div>
+                                    <p>이메일 :</p>
+                                    <p>{userInfo.email}</p>
+                                </div>
+                                <div>
+                                    <p>전화번호 :</p>
+                                    <p>{userInfo.phoneNumber}</p>
+                                </div>
+                                <div>
+                                    <p>권한 :</p>
+                                    <p>{userInfo.role === "USER" ? '사용자' :
+                                        userInfo.role === "OWNER" ? '점주' :
+                                            userInfo.role === "admin" ? '관리자' : '권한 정보가 없습니다'}</p>
+                                </div>
                             </div>
-                            <div>
-                                <p>아이디 :</p>
-                                <p>{userInfo.username}</p>
+                            :
+                            <div css={s.modifyProfileInfo}>
+                                <div>
+                                    <p>이름 :</p>
+                                    <input name='name'
+                                        value={modifyUserInfo.name}
+                                        onChange={handleOnInput} />
+                                </div>
+                                <div>
+                                    <p>아이디 : </p>
+                                    <input name='username'
+                                        value={modifyUserInfo.username}
+                                        onChange={handleOnInput} />
+                                </div>
+                                <div>
+                                    <p>닉네임 : </p>
+                                    <input name='nickname'
+                                        value={modifyUserInfo.nickname}
+                                        onChange={handleOnInput} />
+                                </div>
+                                <div>
+                                    <p>이메일 : </p>
+                                    <input name='email'
+                                        value={modifyUserInfo.email}
+                                        onChange={handleOnInput} />
+                                </div>
+                                <div>
+                                    <p>전화번호 :</p>
+                                    <input name='phoneNumber'
+                                        value={modifyUserInfo.phoneNumber}
+                                        onChange={handleOnInput} />
+                                </div>
+                                <div>
+                                    <p>권한 :</p>
+                                    <p>{modifyUserInfo.role === "USER" ? '사용자' :
+                                        modifyUserInfo.role === "OWNER" ? '점주' :
+                                            modifyUserInfo.role === "admin" ? '관리자' : '권한 정보가 없습니다'}</p>
+                                </div>
                             </div>
-                            <div>
-                                <p>닉네임 :</p>
-                                <p>{userInfo.nickname}</p>
-                            </div>
-                            <div>
-                                <p>이메일 :</p>
-                                <p>{userInfo.email}</p>
-                            </div>
-                            <div>
-                                <p>전화번호 :</p>
-                                <p>{userInfo.phoneNumber}</p>
-                            </div>
-                            <div>
-                                <p>권한 :</p>
-                                <p>{userInfo.role === "USER" ? '사용자':
-                                    userInfo.role === "OWNER" ? '점주' :
-                                    userInfo.role === "admin" ? '관리자' : '권한 정보가 없습니다'}</p>
-                            </div>
-                        </div>
-                        :
-                        <div css={s.modifyProfileInfo}>
-                            <div>
-                                <p>이름 :</p>
-                                <input name='name' 
-                                value={modifyUserInfo.name} 
-                                onChange={handleOnInput}/>
-                            </div>
-                            <div>
-                                <p>아이디 : </p>
-                                <input name='username' 
-                                value={modifyUserInfo.username} 
-                                onChange={handleOnInput}/>
-                            </div>
-                            <div>
-                                <p>닉네임 : </p>
-                                <input name='nickname' 
-                                value={modifyUserInfo.nickname} 
-                                onChange={handleOnInput}/>
-                            </div>
-                            <div>
-                                <p>이메일 : </p>
-                                <input name='email' 
-                                value={modifyUserInfo.email} 
-                                onChange={handleOnInput}/>
-                            </div>
-                            <div>
-                                <p>전화번호 :</p>
-                                <input name='phoneNumber' 
-                                value={modifyUserInfo.phoneNumber} 
-                                onChange={handleOnInput}/>
-                            </div>
-                            <div>
-                                <p>권한 :</p>
-                                <p>{modifyUserInfo.role}</p>
-                            </div>
-                        </div>
                     }
-                    
                 </div>
             </div>
         </div>
