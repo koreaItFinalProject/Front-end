ff618e842ec2f209e2ec7bfb794d8fce3dec80e3
hyeon0907 <kimjh597@naver.com>
2024-10-29 17:48:48 +0900

    사용자 관리 css

--- src/page/Manager/ManagerManagementPage/style.js
+++ src/page/Manager/ManagerManagementPage/style.js
@@ -5,30 +5,38 @@ export const mainLayout = css`
     width: 100%;
     height: 100%;
     box-sizing: border-box;
-    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
 `;
-
-export const userInfo = css`
-    width: 100%;
-    height: 100%;
-    border: 1px solid #dbdbdb;
-`
 export const userList = css`
     width: 100%;
     height: 28px;
+    border-collapse: collapse;
     border: 1px solid #dbdbdb;
+    border-top: none;
 
-    tr,
-    td,
-    th {
-        border: 1px solid #dbdbdb;
+    tr:nth-child(1){
+        position: sticky;
+        top: -1px;
+        background-color: #EFECE8;
     }
+    th, td {
+        border: 1px solid #dbdbdb;
+        padding: 10px; // 패딩 추가
+        text-align: center; // 중앙 정렬
+        white-space : nowrap;
+    }
+
+`
+export const listContainer = css`
+    flex-grow: 1;
+    overflow: auto;
+    height: 100%;
 `
 
 export const selectbutton = (role) => css`
     button{
         padding: 3px 6px;
-        margin: 0px 3px;
         :nth-of-type(${role === 'user' ? 1 : 2}) {
             background-color: #8f8f8f;
         }
@@ -45,32 +53,71 @@ export const modalStyles = {
         top: '50%',
         left: '50%',
         padding: '20px',
-        width: '800px',
+        width: '850px',
         height: '500px',
-        overflow: 'hidden',
+        overflow: "hidden",
     },
 };
-export const layout = css`
-    display: flex;
-    box-sizing: border-box;
-    width: 100%;
+export const boxlayout = css`
     height: 100%;
+    width: 100%;
+`
+
+export const selects = css`
+    display: flex;
+    justify-content: space-between; /* 양쪽 끝에 정렬 */
+    height: 30px;
+`
+export const radioContainer = css`
+    display: flex; /* Flexbox 사용 */
+    align-items: center; /* 수직 중앙 정렬 */
+
+    input{
+        height: 20px;
+        width: 30px;
+    }
+`;
+
+export const radioLabel = css`
+    display: flex; /* Flexbox 사용 */
+    align-items: center; /* 수직 중앙 정렬 */
+`;
+
+export const layout2 = css`
+    display: flex;
+    border: 1px solid #dbdbdb;
+    height: calc(100% - 30px);
+    width: 100%;
 `
 export const contentBox = css`
     display: flex;
+    justify-content: space-between;
     flex-direction: column;
     white-space: nowrap;
-    padding-top: 30px;
     div{
-        padding: 30px;
+        border: 1px solid #dbdbdb;
+        border-right: none;
+        border-left: none;
+        padding: 20px;
+
+        :nth-child(1){
+            border-top: none;
+        }
+
+        :nth-last-child(1){
+            border-bottom: none;
+        }
+        :hover{
+            background-color: #a5a5a5;
+        }
+        :active{
+            background-color: #7e7e7e;
+        }
     }
 `
 
 export const content = css`
-    margin-top: 15px;
-    margin-left: 10px;
     width: 100%;
-    height: 80%;
-    border: 1px solid #dbdbdb;
+    border-left: 1px solid #dbdbdb;
     box-sizing: border-box;
 `
