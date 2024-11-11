5dc343f0d4172f6dc8306c86a6683319792918e5
ehddls98 <icbku1357@naver.com>
2024-11-11 14:14:34 +0900

    Feat: 관리자 페이지 대시보드 작업중

--- src/page/Admin/WebManager/ManagerDashBoardPage/ManagerDashBoardPage.jsx
+++ src/page/Admin/WebManager/ManagerDashBoardPage/ManagerDashBoardPage.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-
-
-
+/** @jsxImportSource @emotion/react */
+import * as s from "./style";
 
 function ManagerDashBoardPage(props) {
-    // return (
-
-    // );
+    return (
+        <div css={s.layout}>
+            <div css={s.row}>
+                <div css={s.column}>
+                    <h3>최근 리뷰</h3>
+                    <table css={s.cafeListTable}>
+                        <tr>
+                            <th>순번</th>
+                            <th>작성자</th>
+                            <th>카페명</th>
+                            <th>내용</th>
+                            <th>평점</th>
+                            <th>작성일</th>
+                        </tr>
+                        <tr>
+                        </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                    </table>
+                </div>
+            </div>
+            <div css={s.row}>
+            </div>
+        </div>
+    );
 }
 
 export default ManagerDashBoardPage;
